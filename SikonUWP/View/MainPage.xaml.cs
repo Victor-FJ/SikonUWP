2b3f29c5036237c0297342d663c8570919b80a4a
Victor Friis-Jensen <vict5481@edu.easj.dk>
2020-04-23 12:49:37 +0200

    Work on Event Editor Page

--- SikonUWP/View/MainPage.xaml.cs
+++ SikonUWP/View/MainPage.xaml.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Globalization;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using SikonUWP.Persistency;
+using SikonUWP.ViewModel;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -16,31 +23,37 @@ namespace SikonUWP.View
         public MainPage()
         {
             this.InitializeComponent();
-            Open();
+            //Pass the to controls to the viewmodel
+            MainViewModel viewModel = new MainViewModel(ContentFrame, NavigationView);
+            this.DataContext = viewModel;
+            //Changes the language to be danish
+            ApplicationLanguages.PrimaryLanguageOverride = "da-DK";
+            SetNavigationViewTags();
         }
 
-        private async void Open()
-        {
-            bool ok = await PersistencyManager.Tester();
-        }
 
-       
-        
-        // Denne del af koden navigere til en side i Viewet 
+        //Navigere til en side i viewet når brugeren vælger et item i navigationviewet
         private void NavigationView_OnItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
         {
-            string tag = (string) args.InvokedItemContainer.Tag;
-            Navigate(tag);
+            if (args.InvokedItemContainer.Tag is Type pageType)
+                ContentFrame.Navigate(pageType);
         }
 
-        private void Navigate(string pageName)
+        private void AdjustTitlebarColor()
         {
-            Type pageType = Assembly.GetExecutingAssembly().GetType($"SikonUWP.View.{pageName}");
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            Color backGroundColor = Color.FromArgb(255, 173, 216, 230);
+            titleBar.BackgroundColor = backGroundColor;
+            titleBar.ButtonBackgroundColor = backGroundColor;
+        }
 
-            if (pageType == null)
-                return;
-
-            ContentFrame.Navigate(pageType);
+        private void SetNavigationViewTags()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            foreach (object menuItem in NavigationView.MenuItems)
+                if (menuItem is NavigationViewItem navigationViewItem
+                    && navigationViewItem.Tag is string naviPageType)
+                    navigationViewItem.Tag = assembly.GetType($"SikonUWP.View.{naviPageType}");
         }
     }
 }
