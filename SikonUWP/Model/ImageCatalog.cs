511194a5ca41f980acdbd1af632b46b9c094b3f2
Victor Friis-Jensen <vict5481@edu.easj.dk>
2020-05-05 13:08:29 +0200

    Image Test

--- SikonUWP/Model/ImageCatalog.cs
+++ SikonUWP/Model/ImageCatalog.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Windows.Storage;
 using Windows.Storage.Pickers;
@@ -9,18 +9,30 @@ using Windows.Storage.Streams;
 using Windows.UI.Xaml.Media.Imaging;
 using SikonUWP.Persistency;
 
-namespace SikonUWP.Handlers
+namespace SikonUWP.Model
 {
-    public static class ImageHandler
+    public class ImageCatalog
     {
-        private static StorageFolder _imageFolder;
-        public static Dictionary<string, BitmapImage> Dictionary { get; set; }
+        public ReadOnlyDictionary<string, BitmapImage> Dictionary { get; set; }
+
+        private Dictionary<string, BitmapImage> _dictionary;
+        private StorageFolder _imageFolder;
+
+
+        public ImageCatalog()
+        {
+            _dictionary = new Dictionary<string, BitmapImage>();
+            Dictionary = new ReadOnlyDictionary<string, BitmapImage>(_dictionary);
+        }
+
+
+        #region Methods
 
         /// <summary>
         /// Downloads and deletes images so the locally saved images are synchronized with the database
         /// </summary>
         /// <returns></returns>
-        public static async Task SyncImages()
+        public async Task SyncImages()
         {
             //Gets image names from database
             List<string> imageNames = await ImagePersistence.GetNames();
@@ -31,13 +43,14 @@ namespace SikonUWP.Handlers
             Dictionary<string, StorageFile> localImageDictionary = files.ToDictionary(x => x.Name, x => x);
 
             //Synchronising local storage with database
-            Dictionary = new Dictionary<string, BitmapImage>();
+            _dictionary = new Dictionary<string, BitmapImage>();
+            Dictionary = new ReadOnlyDictionary<string, BitmapImage>(_dictionary);
             foreach (string imageName in imageNames)
             {
                 //If an image from the database is already saved locally then we do nothing
                 if (localImageDictionary.ContainsKey(imageName))
                 {
-                    Dictionary.Add(imageName, await AsBitmapImage(localImageDictionary[imageName]));
+                    _dictionary.Add(imageName, await AsBitmapImage(localImageDictionary[imageName]));
                     localImageDictionary.Remove(imageName);
                 }
                 else
@@ -46,7 +59,7 @@ namespace SikonUWP.Handlers
                     StorageFile file = await _imageFolder.CreateFileAsync(imageName, CreationCollisionOption.FailIfExists);
                     byte[] pixelBytes = await ImagePersistence.Get(imageName);
                     await FileIO.WriteBytesAsync(file, pixelBytes);
-                    Dictionary.Add(imageName, await AsBitmapImage(file));
+                    _dictionary.Add(imageName, await AsBitmapImage(file));
                 }
             }
 
@@ -60,7 +73,7 @@ namespace SikonUWP.Handlers
         /// Opens a filepicker so the user can find and pick an image for the program
         /// </summary>
         /// <returns>The image as an StorageFile</returns>
-        public static async Task<StorageFile> PickSingleImage()
+        public async Task<StorageFile> PickSingleImage()
         {
             //Great filepicker for picking an image
             FileOpenPicker openPicker = new FileOpenPicker();
@@ -77,15 +90,18 @@ namespace SikonUWP.Handlers
         /// Tries to add an image to the database as well as saving it locally
         /// </summary>
         /// <param name="file">The StorageFile containing the image</param>
+        /// <param name="newName">The new name of the image</param>
         /// <returns>Boolean value that idecates whether the image was saved successfully</returns>
-        public static async Task<bool> AddImage(StorageFile file)
+        public async Task<bool> AddImage(StorageFile file, string newName)
         {
+            if (string.IsNullOrWhiteSpace(newName))
+                newName = file.Name;
             byte[] pixelBytes = await AsByteArray(file);
-            bool ok = await ImagePersistence.Post(file.Name, pixelBytes);
+            bool ok = await ImagePersistence.Post(newName, pixelBytes);
             if (ok)
             {
-                StorageFile copiedFile = await file.CopyAsync(_imageFolder);
-                Dictionary.Add(file.Name, await AsBitmapImage(copiedFile));
+                StorageFile copiedFile = await file.CopyAsync(_imageFolder, newName);
+                _dictionary.Add(newName, await AsBitmapImage(copiedFile));
             }
 
             return ok;
@@ -96,14 +112,14 @@ namespace SikonUWP.Handlers
         /// </summary>
         /// <param name="fileName">The name of the file</param>
         /// <returns></returns>
-        public static async Task<bool> RemoveImage(string fileName)
+        public async Task<bool> RemoveImage(string fileName)
         {
             bool ok = await ImagePersistence.Delete(fileName);
             if (ok)
             {
                 StorageFile file = await _imageFolder.GetFileAsync(fileName);
                 await file.DeleteAsync();
-                Dictionary.Remove(fileName);
+                _dictionary.Remove(fileName);
             }
 
             return ok;
@@ -114,7 +130,7 @@ namespace SikonUWP.Handlers
         /// </summary>
         /// <param name="file">The storageFile that contains the image</param>
         /// <returns>The bitmapImage</returns>
-        public static async Task<BitmapImage> AsBitmapImage(StorageFile file)
+        public async Task<BitmapImage> AsBitmapImage(StorageFile file)
         {
             if (file != null)
             {
@@ -134,7 +150,7 @@ namespace SikonUWP.Handlers
         /// </summary>
         /// <param name="file">The StorageFile containing the image</param>
         /// <returns>The byte array with the image data</returns>
-        public static async Task<byte[]> AsByteArray(StorageFile file)
+        public async Task<byte[]> AsByteArray(StorageFile file)
         {
             using (IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read))
             {
@@ -147,5 +163,7 @@ namespace SikonUWP.Handlers
                 return pixelBytes;
             }
         }
+
+        #endregion
     }
 }
--- SikonUWP/ViewModel/EventHomeViewModel.cs
+++ SikonUWP/ViewModel/EventHomeViewModel.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using ModelLibrary.Model;
+using SikonUWP.Annotations;
+using SikonUWP.Model;
 
 namespace SikonUWP.ViewModel
 {
-    class EventHomeViewModel
+    public class EventHomeViewModel : INotifyPropertyChanged
     {
+        public Event FirstTestEvent { get; set; }
+
+        public EventHomeViewModel()
+        {
+            Load();
+        }
+
+        private async void Load()
+        {
+            await EventSingleton.Instance.EventCatalog.Load();
+            FirstTestEvent = EventSingleton.Instance.EventCatalog.Collection[0];
+            OnPropertyChanged(nameof(FirstTestEvent));
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
